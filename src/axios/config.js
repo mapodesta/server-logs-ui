ffa73abf59cd2a435ea0893499998dc450edb41c
Mathias <mathias643@gmail.com>
2021-10-02 17:21:51 -0300

    change-url

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  //baseURL: 'https://server-log-api.herokuapp.com/api/users',
-  baseURL: 'http://localhost:8080/api/users',
+  baseURL: 'https://server-log-api.herokuapp.com/api/users',
+  //baseURL: 'http://localhost:8080/api/users',
 });
